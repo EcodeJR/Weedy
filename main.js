3b861a8f5a6230f35e7897432b01f31640ae3915
Emmanuel Dalyop <108156886+EcodeJR@users.noreply.github.com>
2022-11-16 13:15:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,102 @@ let conta = document.getElementById('container');
 let search_bx = document.getElementById('search_input');
 let search_btn = document.getElementById('btn');
 
+
+
+//clock
+let time = document.getElementById('time');
+let day0 = document.getElementById('day');
+
+setInterval(() => {
+    let d = new Date();
+    time.innerHTML = d.toLocaleTimeString();
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    day0.innerHTML = weekday[d.getDay()];
+}, 1000);
+
+
+//weather app
+
+/*
+search_btn.addEventListener('click', function() {
+    
+})*/
+
+
+
+
+//geting user location
+const findmylocation = () => {
+    let city = document.getElementById('city');
+
+    const success = (position) => {
+        console.log(position);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+
+        fetch(locationApi)
+            .then(responde => responde.json())
+            .then(data => {
+                city.innerHTML = data.city
+            })
+
+    }
+
+
+    const error = () => {
+        city.innerHTML = 'Unable to get location';
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+
+
+}
+
+
+
+//weather
+let weatherApp = {
+    "apiKey": "fe456a7eb185bad555b83268ec33960e",
+    fetchWeather: function(city) {
+        fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey)
+            .then((res) => res.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon } = data.weather[0];
+        const { temp } = data.main;
+        city.innerHTML = name;
+        document.getElementById('degree').innerHTML = temp + "°C";
+        document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector('.weather_info').classList.remove('loading');
+        document.getElementById('body1').style.background = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function() {
+        this.fetchWeather(search_bx.value);
+    }
+
+};
+
+function search_image() {
+    weatherApp.search();
+    city.innerHTML = search_bx.value;
+    search_bx.value = '';
+};
+/*
+search_btn.addEventListener('keyup', function() {
+    if (event.key == "Enter") {
+        weatherApp.search();
+    }
+});*/
+
+/*
 let search_val = 'asia';
 
 
@@ -32,62 +128,7 @@ $.ajax({
 
     }
 })
-
-
-//clock
-let time = document.getElementById('time');
-let day0 = document.getElementById('day');
-
-setInterval(() => {
-    let d = new Date();
-    time.innerHTML = d.toLocaleTimeString();
-    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    day0.innerHTML = weekday[d.getDay()];
-}, 1000);
-
-
-//weather app
-let city = document.getElementById('city');
-/*
-search_btn.addEventListener('click', function() {
-    
-})*/
-
-
-function search_image() {
-    city.innerHTML = search_bx.value;
-    search_bx.value = '';
-};
-search_val = search_bx.value;
-
-
-//geting user location
-const findmylocation = () => {
-
-
-    const success = (position) => {
-        console.log(position);
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-
-        const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
-
-        fetch(locationApi)
-            .then(res => res.json())
-            .then(data => {
-                city.innerHTML = data.city
-            })
-
-    }
-
-
-    const error = () => {
-        city.innerHTML = 'Unadle to get Location';
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error);
-
-
-}
+*/
 
 window.addEventListener('load', findmylocation);
+window.addEventListener('load', weatherApp.fetchWeather(city.innerHTML));
